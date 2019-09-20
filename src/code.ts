c5027b60039434d290bff359a1594a9bcbe2a557
Edson Machado <edson.machado@soprasteria.com>
2019-09-20 11:12:54 +0200

    Hot vs. Cold Observables

--- src/code.ts
+++ src/code.ts
@@ -1,32 +1,12 @@
-import { Observable } from 'rxjs/Observable';
+import { fromEvent } from 'rxjs/Observable/fromEvent';
 
-const observable = Observable.create((observer: any) => {
-  try {
-    observer.next('Learn RxJS!');
-    observer.next('Streams, Observables & Subscriptions');
-    setInterval(() => {
-      observer.next('Interval');
-    }, 2000);
-  } catch (err) {
-    observer.error(err);
-  }
-});
-
-const observer = observable.subscribe(
-  (x: any) => addItem(x),
-  (error: any) => addItem(error),
-  () => addItem('Completed'),
-);
-
-const observer2 = observable.subscribe(
-  (x: any) => addItem(x)
-);
-
-observer.add(observer2);
+const observable = fromEvent(document, 'mousemove');
 
 setTimeout(() => {
-  observer.unsubscribe();
-}, 6001);
+  observable.subscribe(
+    (x: any) => addItem(x)
+  )
+}, 2000);
 
 function addItem(val: any) {
   const node = document.createElement('li');
