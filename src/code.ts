550f1561726938a19c034a370902adebe115ae26
Edson Machado <edson.machado@soprasteria.com>
2019-09-23 08:34:34 +0200

    Subjects

--- src/code.ts
+++ src/code.ts
@@ -1,12 +1,27 @@
-import { fromEvent } from 'rxjs/Observable/fromEvent';
+import { Subject } from 'rxjs/Subject';
 
-const observable = fromEvent(document, 'mousemove');
+const subject = new Subject();
 
-setTimeout(() => {
-  observable.subscribe(
-    (x: any) => addItem(x)
-  )
-}, 2000);
+subject.subscribe(
+  data => addItem('Observer 1: ' + data),
+  err => addItem(err),
+  () => addItem('Observer 1 Completed')
+)
+
+subject.next('The first thing has been sent');
+
+const observer2 = subject.subscribe(
+  data => addItem('Observer 2: ' + data),
+  err => addItem(err),
+  () => addItem('Observer 2 Completed')
+)
+
+subject.next('The second thing has been sent');
+subject.next('A third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('A final thing has been sent');
 
 function addItem(val: any) {
   const node = document.createElement('li');
